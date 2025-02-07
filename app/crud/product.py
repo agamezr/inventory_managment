7cc1a6f445515bae68a096ff2acc79b0b1918b6e
Alejandro Gámez <ae.gamezr@gmail.com>
2025-02-07 01:07:16 -0600

    adding filtering by category and price

--- app/crud/product.py
+++ app/crud/product.py
@@ -1,9 +1,24 @@
 from sqlalchemy.orm import Session
 from app.models.product import Product
 
-def get_all_products(db: Session, page: int = 0, per_page: int = 10):
-    return db.query(
-                Product).limit(per_page).offset(
+def get_all_products(
+        db: Session,
+        page: int = 0,
+        per_page: int = 10,
+        category: str = None, 
+        min_price: float = None, 
+        max_price: float = None, 
+        ):
+    products = db.query(Product)
+
+    if category:
+        products = products.filter(Product.category == category)
+    if min_price is not None:
+        products = products.filter(Product.price >= min_price)
+    if max_price is not None:
+        products = products.filter(Product.price <= max_price)
+
+    return products.limit(per_page).offset(
                     page - 1
                     if page == 1
                     else (page - 1) * per_page
--- app/routes/product.py
+++ app/routes/product.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.crud.product import get_all_products
 from app.schemas.product import ProductSchema
 from app.config.dependencies import get_db
-from typing import List
+from typing import List, Optional
 
 product = APIRouter()
 
@@ -11,6 +11,10 @@ product = APIRouter()
 def read_products(
         db: Session = Depends(get_db),
         page: int = Query(ge=1, default=1, required=False),
-        per_page: int = Query(ge=1, le=100, default=10, required=False)
+        per_page: int = Query(ge=1, le=100, default=10, required=False),
+        category: Optional[str] = Query(None, description="Categoría del producto"),
+        min_price: Optional[float] = Query(None, description="Precio mínimo"),
+        max_price: Optional[float] = Query(None, description="Precio máximo")
     ):
-    return get_all_products(db, page, per_page)
+    return get_all_products(db, page, per_page, category, min_price, max_price)
+
