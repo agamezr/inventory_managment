e001ba31d31645ed1599289fb10fb278e23177b4
Alejandro Gámez <ae.gamezr@gmail.com>
2025-02-07 00:41:53 -0600

    adding simple pagination

--- app/crud/product.py
+++ app/crud/product.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import Session
 from app.models.product import Product
 
-def get_all_products(db: Session):
-    return db.query(Product).all()
+def get_all_products(db: Session, page: int = 0, per_page: int = 10):
+    return db.query(
+                Product).limit(per_page).offset(
+                    page - 1
+                    if page == 1
+                    else (page - 1) * per_page
+                ).all()
--- app/routes/product.py
+++ app/routes/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from app.crud.product import get_all_products
 from app.schemas.product import ProductSchema
@@ -8,6 +8,9 @@ from typing import List
 product = APIRouter()
 
 @product.get("/products", response_model=List[ProductSchema])
-def read_products(db: Session = Depends(get_db)):
-    return get_all_products(db)
-
+def read_products(
+        db: Session = Depends(get_db),
+        page: int = Query(ge=1, default=1, required=False),
+        per_page: int = Query(ge=1, le=100, default=10, required=False)
+    ):
+    return get_all_products(db, page, per_page)
