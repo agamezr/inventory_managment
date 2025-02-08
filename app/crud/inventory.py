0e4f5b25110426711f8a0d9bd08754e95399cee1
Alejandro Gámez <ae.gamezr@gmail.com>
2025-02-08 02:50:30 -0600

    adding SQLAlchemy block transactions.

--- app/crud/inventory.py
+++ app/crud/inventory.py
@@ -12,58 +12,61 @@ def get_inventory_by_store(db: Session, store_id: str):
     return db.query(Inventory).options(joinedload(Inventory.product)).filter(Inventory.store_id == store_id).all()
 
 def transfer_inventory_products(db: Session, transfer_params: InventoryTransferSchema):
-    origin_inventory_data = db.query(Inventory).filter(
-            Inventory.store_id == transfer_params.origin_store_id,
-            Inventory.product_id == transfer_params.product_id
-        ).first()
-
-    if not origin_inventory_data:
-        raise HTTPException(status_code=404, detail="Product not found in origin store!")
-
-    if origin_inventory_data.quantity < transfer_params.quantity:
-        raise HTTPException(status_code=400, detail="Not enough stock in origin store!")
-
-    if (origin_inventory_data.quantity - transfer_params.quantity) < origin_inventory_data.min_stock:
-        raise HTTPException(status_code=400, detail="Not enough min stock in origin store!")
-
-    target_inventory_data = db.query(Inventory).filter(
-            Inventory.store_id == transfer_params.target_store_id,
-            Inventory.product_id == transfer_params.product_id
-        ).first()
-    
-    if not target_inventory_data:
-        target_inventory_data = Inventory(
-            id = str(uuid.uuid4()),
-            product_id = transfer_params.product_id,
-            store_id = transfer_params.target_store_id,
-            quantity = 0,
-            min_stock = 5,
-        )
-        db.add(target_inventory_data)
-        db.flush()
-
-    origin_inventory_data.quantity -= transfer_params.quantity
-    target_inventory_data.quantity += transfer_params.quantity
-
-    movement = Movement(
-        id = str(uuid.uuid4()),
-        product_id = transfer_params.product_id,
-        source_store_id = transfer_params.origin_store_id,
-        target_store_id = transfer_params.target_store_id,
-        quantity = transfer_params.quantity,
-        timestamp = datetime.utcnow(),
-        type = MovementType.TRANSFER
-    )
-    db.add(movement)
-
     try:
-        db.commit()
+        with db.begin():
+            origin_inventory_data = db.query(Inventory).filter(
+                    Inventory.store_id == transfer_params.origin_store_id,
+                    Inventory.product_id == transfer_params.product_id
+                ).first()
+
+            if not origin_inventory_data:
+                raise HTTPException(status_code=404, detail="Product not found in origin store!")
+
+            if origin_inventory_data.quantity < transfer_params.quantity:
+                raise HTTPException(status_code=400, detail="Not enough stock in origin store!")
+
+            if (origin_inventory_data.quantity - transfer_params.quantity) < origin_inventory_data.min_stock:
+                raise HTTPException(status_code=400, detail="Not enough min stock in origin store!")
+
+            target_inventory_data = db.query(Inventory).filter(
+                    Inventory.store_id == transfer_params.target_store_id,
+                    Inventory.product_id == transfer_params.product_id
+                ).first()
+            
+            if not target_inventory_data:
+                target_inventory_data = Inventory(
+                    id = str(uuid.uuid4()),
+                    product_id = transfer_params.product_id,
+                    store_id = transfer_params.target_store_id,
+                    quantity = 0,
+                    min_stock = 5,
+                )
+                db.add(target_inventory_data)
+                db.flush()
+
+            origin_inventory_data.quantity -= transfer_params.quantity
+            target_inventory_data.quantity += transfer_params.quantity
+
+            movement = Movement(
+                id = str(uuid.uuid4()),
+                product_id = transfer_params.product_id,
+                source_store_id = transfer_params.origin_store_id,
+                target_store_id = transfer_params.target_store_id,
+                quantity = transfer_params.quantity,
+                timestamp = datetime.utcnow(),
+                type = MovementType.TRANSFER
+            )
+            db.add(movement)
+
+        return {"message": "Transfer successful!"}
+
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=500, detail="Database error during transfer!")
-
-    return {"message": "Transfer successful!"}
-
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    
 def get_low_stock_products(db: Session):
     return db.query(
         Product.id,
--- app/crud/product.py
+++ app/crud/product.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.product import Product
 from app.models.inventory import Inventory
@@ -54,50 +55,57 @@ def get_product_by_id(db: Session, id: str):
     ).outerjoin(Inventory).filter(Product.id == id).group_by(Product.id).first()
 
 def create_product(db: Session, product_params: ProductNewSchema):
-    existing_product = db.query(Product).filter(Product.sku == product_params.sku).first()
-    if existing_product:
-        raise ValueError("SKU already exists.")
-    
-    product = Product(
-        id = str(uuid.uuid4()),
-        name = product_params.name,
-        description = product_params.description,
-        category = product_params.category,
-        price = product_params.price,
-        sku = product_params.sku
-    )
-
     try:
-        db.add(product)
-        db.commit()
+        with db.begin():
+            existing_product = db.query(Product).filter(Product.sku == product_params.sku).first()
+            if existing_product:
+                raise ValueError("SKU already exists!")
+            
+            product = Product(
+                id = str(uuid.uuid4()),
+                name = product_params.name,
+                description = product_params.description,
+                category = product_params.category,
+                price = product_params.price,
+                sku = product_params.sku
+            )
+            db.add(product)
+        
         db.refresh(product)
         return product
+
     except IntegrityError:
         db.rollback()
-        raise ValueError("Databse error: Unable to create product.")
-    
+        raise ValueError("Databse error: Unable to create product!")
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
 def update_product(db: Session, id: str, product_params: ProductNewSchema):
-    product = db.query(Product).filter(Product.id == id).first()
-
-    print(product)
-    if not product:
-        return None
-
-    if product_params.sku:
-        existing_product = db.query(Product).filter(Product.sku == product_params.sku).first()
-        if existing_product and existing_product.id != product.id:
-            raise ValueError("SKU already exists.")
-    
-    for field, value in product_params.dict(exclude_unset=True).items():
-        setattr(product, field, value)
-
     try:
-        db.commit()
+        with db.begin():
+            product = db.query(Product).filter(Product.id == id).first()
+
+            print(product)
+            if not product:
+                return None
+
+            if product_params.sku:
+                existing_product = db.query(Product).filter(Product.sku == product_params.sku).first()
+                if existing_product and existing_product.id != product.id:
+                    raise ValueError("SKU already exists!")
+            
+            for field, value in product_params.dict(exclude_unset=True).items():
+                setattr(product, field, value)
+
         db.refresh(product)
         return product
     except IntegrityError:
         db.rollback()
-        raise ValueError("Database error: Unable to update product.")
+        raise ValueError("Database error: Unable to update product!")
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
     
 def delete_product(db: Session, product_id: str):
     product = db.query(Product).filter(Product.id == product_id).first()
@@ -111,4 +119,4 @@ def delete_product(db: Session, product_id: str):
         return product
     except IntegrityError:
         db.rollback()
-        raise ValueError("Database error: Unable to delete product.")
+        raise ValueError("Database error: Unable to delete product!")
