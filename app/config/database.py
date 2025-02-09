5364482e42724742c9ab51b69ed840366caa9ce6
Alejandro Gámez <ae.gamezr@gmail.com>
2025-02-09 13:00:41 -0600

    Update product.py

--- app/config/database.py
+++ app/config/database.py
@@ -6,10 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-
 engine = create_engine(DATABASE_URL)
 
 meta = MetaData()
-conn = engine.connect()
+# conn = engine.connect()
 Base = declarative_base()
